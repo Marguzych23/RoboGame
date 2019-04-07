fa847e7bf19af5591e50593116d2a83859106b6a
Marguzych <zaynullin.emil@gmail.com>
2019-04-07 20:21:46 +0300

    refactor

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,7 +4,25 @@
 namespace App\Controller;
 
 
-class AuthController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthController extends AbstractController
 {
+    /**
+     * @Route("/login", name="login")
+     * @return Response
+     */
+    public function index()
+    {
+//        return $this->render('auth/index.html.twig');
+        return new Response('Auth page');
+    }
+
+    public function auth(string $nickName)
+    {
+        return $this->redirectToRoute('homepage', array('nickName' => $nickName));
+    }
 
 }
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,7 +4,19 @@
 namespace App\Controller;
 
 
-class IndexController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
 {
+
+    /**
+     * @Route("/index", name="homepage")
+     * @return Response
+     */
+    public function index() {
+        return new Response('HOMEPAGE');
+    }
 
 }
